450a571d1093efa86c2c41216c4163074cb69f98
JhonWeyffer <jhonatasweyffer@gmail.com>
2025-12-16 01:55:11 -0400

    Correções de bugs

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ app.post("/forgot", (req, res) => {
 });
 
 // Iniciar servidor
-app.listen(3000, () => {
-  console.log(`Servidor rodando na porta 3000`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
